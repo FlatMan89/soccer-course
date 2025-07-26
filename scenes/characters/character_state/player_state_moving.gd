43341f556e05b4ca1ae7a3d89fc7dd6f2d59ffe5
FlatMan <yehunn@qq.com>
2025-07-27 04:05:35 +0800

    add: 添加了基于节点的状态机

---
+++ scenes/characters/character_state/player_state_moving.gd
@@ -0,0 +1,19 @@
+class_name PlayerStateMoving
+extends PlayerState
+
+func _process(_delta: float) -> void:
+	if player.control_scheme == player.ControlScheme.CPU:
+		pass
+	else:
+		handle_human_movement()	
+	
+	player.set_movement_animation()
+	player.set_heading()
+	
+	
+func handle_human_movement() -> void:
+	var directioin := KeyUtils.get_input_vector(player.control_scheme)
+	player.velocity = directioin * player.speed
+	
+	if player.velocity != Vector2.ZERO and KeyUtils.is_action_just_pressed(player.control_scheme, KeyUtils.Action.SHOOT):
+		state_transition_requested.emit(Player.State.TACKLING)
