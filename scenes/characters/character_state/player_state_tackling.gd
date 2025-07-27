d377a66a81d575fa55927bc176567041c28077fe
FlatMan <yehunn@qq.com>
2025-07-27 13:13:31 +0800

    add: 添加铲球时的摩擦力

--- scenes/characters/character_state/player_state_tackling.gd
+++ scenes/characters/character_state/player_state_tackling.gd
@@ -1,16 +1,27 @@
 class_name PlayerStateTackling
 extends PlayerState
 
-const DURATION_TACKLE := 200
+# 铲球后回复时间
+const DURATION_PRIOR_RECOVERY := 200
+# 铲球摩擦力
+const GROUND_FRICTION := 250
+# 铲球是否已完成
+var is_trackle_complete := false
+# 铲球完成时间
+var time_finish_tackle := Time.get_ticks_msec()
 
-var time_start_tackle := Time.get_ticks_msec()
-
-
-func _process(_delta: float) -> void:
-	if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
-		state_transition_requested.emit(Player.State.RECOVERING)
-	
-	
 func _enter_tree() -> void:
 	animation_player.play("tackle")
-	time_start_tackle = Time.get_ticks_msec()
+
+func _process(delta: float) -> void:
+	# 如何铲球未完成
+	if not is_trackle_complete:
+		# 将玩家的速度缓慢降为零
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, delta * GROUND_FRICTION)
+		# 如果玩家铲球速度降为了零，标记铲球已完成并开始计时铲球结束到恢复的时间
+		if player.velocity == Vector2.ZERO:
+			is_trackle_complete = true
+			time_finish_tackle = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - time_finish_tackle > DURATION_PRIOR_RECOVERY:
+		# 计时器到达铲球时间后进入恢复状态
+		state_transition_requested.emit(Player.State.RECOVERING)
